e314baaae425ebad9f07411c80c4570219a3046c
남우석 <nws94@douzone.com>
2023-03-08 00:11:50 +0900

    데이터베이스 객체모델 동기화 소스 추가

--- database/global-stat.model.js
+++ database/global-stat.model.js
@@ -10,14 +10,55 @@ module.exports = (sequelize) => {
                 allowNull: false,                   // 빈 값 허용 X
                 primaryKey: true                    // 기본키로 지정
             },
-            cc: {
+            cc: { // 국가코드(cc는 country code의 약자)
                 type: DataTypes.CHAR(2),
                 allowNull: false
             },
-            date: {
+            date: { // 날짜
                 type: DataTypes.DATEONLY,
                 allowNull: false
             },
+            confirmed: { // 확진자 수
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            death: { // 사망자 수
+                type: DataTypes.INTEGER,
+                allowNull: true
+            },
+            released: { // 완치자 수
+                type: DataTypes.INTEGER,
+                allowNull: true
+            },
+            tested: { // 총 검사자 수
+                type: DataTypes.INTEGER,
+                allowNull: true
+            },
+            testing: { // 검사중 수
+                type: DataTypes.INTEGER,
+                allowNull: true
+            },
+            negative: { // 결과 음성 수
+                type: DataTypes.INTEGER,
+                allowNull: true
+            }
+        },
+        {
+            sequelize,                  // 시퀄라이즈 인스턴스
+            tableName: 'GlobalStat',    // 데이터베이스에서 테이블의 이름
+            indexes: [                  // 테이블 인덱스
+                {
+                    name: 'PRIMARY',
+                    unique: true,
+                    fields: [{name: 'id'}]
+                },
+                {
+                    name: 'ccWithDate',
+                    unique: true,
+                    fields: [{name: 'cc'}, {name: 'date'}]
+                }
+            ],
+            timestamps: false,          // 타임스태탬프 속성 자동 생성 X
         }
     )
 }
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const {sequelize} = require('./database');
 
-app.use(bodyParser.json());
+async function launchServer() {
+    const app = express();
+    app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-    res.json({message : 'Hello CoronaBoard!'});
-})
+    app.get('/', (req, res) => {
+        res.json({message: 'Hello CoronaBoard!'});
+    })
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+    try {
+        await sequelize.sync();
+        console.log('Database is ready!');
+    } catch (error) {
+        console.log('Unable to connect to the database:');
+        console.log(error);
+        process.exit(1);
+    }
+
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    })
+}
+
+launchServer();
+
